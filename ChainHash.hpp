affe5628fbb2194b09d2b70a81002c19f978997c
Artem Aroslankin <adaroslankin@gmail.com>
2020-12-15 01:41:26 +0300

    Initial commit

---
+++ ChainHash.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Артем Аросланкин on 15.12.2020.
+//
+
+#ifndef HASHING_CHAINHASH_HPP
+#define HASHING_CHAINHASH_HPP
+
+#pragma once
+
+template<typename DataType>
+class HashTable {
+    
+};
+
+#endif //HASHING_CHAINHASH_HPP
